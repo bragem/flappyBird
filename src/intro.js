daaed97b46c7b85b1f00d2ce08835d4e13b56756
Brage <bragmin@hotmail.com>
2021-10-17 20:56:52 +0200

    Created logic that stores a given name and changes to the correct window afterwards

---
+++ src/intro.js
@@ -0,0 +1,18 @@
+let form = document.forms["nameForm"];
+
+//if a name is already stored on startup, the window is automatically changed to the game
+if (window.localStorage.getItem("name") != null) window.location.replace("/src/game.html");
+
+
+
+form.onsubmit = function (e){
+    //When forms are submitted the page is refreshed by default,
+    //this circumvents that
+    e.preventDefault();
+
+    //saves the name and changes window
+    let name = form["name"].value;
+    window.localStorage.setItem("name",name);
+
+    window.location.replace("/src/game.html");
+}
